41265679102cf2901033da1297cae05047d3dfe1
Gurkan26 <gurkan683@gmail.com>
2021-02-07 16:16:05 +0300

    tez analiz

---
+++ TezAnaliz/TezAnaliz/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace TezAnaliz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelyol = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::WindowsFormsApp4.Properties.Resources.analiz;
+            this.button1.Location = new System.Drawing.Point(670, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 85);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(92, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Dosyayı Seç";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(25, 161);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(795, 279);
+            this.richTextBox1.TabIndex = 21;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Image = global::WindowsFormsApp4.Properties.Resources.ekle2;
+            this.button2.Location = new System.Drawing.Point(90, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 85);
+            this.button2.TabIndex = 22;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(667, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Dosyayı Tara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(307, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 27);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Tez Analiz Uygulaması";
+            // 
+            // labelyol
+            // 
+            this.labelyol.AutoSize = true;
+            this.labelyol.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelyol.Location = new System.Drawing.Point(245, 95);
+            this.labelyol.Name = "labelyol";
+            this.labelyol.Size = new System.Drawing.Size(0, 16);
+            this.labelyol.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.hand_painted_watercolor_background_with_sky_clouds_shape_24972_1095;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(849, 449);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelyol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelyol;
+    }
+}
+
---
+++ TezAnaliz/TezAnaliz/Form1.cs
@@ -0,0 +1,277 @@
+﻿using Spire.Doc;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TezAnaliz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = System.Windows.Forms.HorizontalAlignment.Center;
+       
+        }
+
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "URL KONTROL EDİLİYOR..." + Environment.NewLine;
+            richTextBox1.Text = richTextBox1.Text + "BAŞLIKLAR KONTROL EDİLİYOR" + Environment.NewLine;
+            richTextBox1.Text = richTextBox1.Text + "YAZI TİPİ KONTROL EDİLİYOR" + Environment.NewLine;
+            richTextBox1.Text = richTextBox1.Text + "SAYFA DÜZENİ KONTROL EDİLİYOR" + Environment.NewLine;
+            richTextBox1.Text = richTextBox1.Text + "----KONTROL İŞLEMİ TAMAMLANDI----" + Environment.NewLine;
+            richTextBox1.Text = richTextBox1.Text + "__ _HATALAR_ __" + Environment.NewLine;
+            Document doc = new Document();
+            bool value = false;
+            try
+            {
+                doc.LoadFromFile(@""+labelyol.Text);
+                value = false;
+            }
+            catch
+            {
+                MessageBox.Show(".Docx uzantılı bir dosya giriniz.");
+                value = true;
+            }
+
+            if (!value)
+            {
+                List<YaziTipiBilgisi> HeadInfos = new List<YaziTipiBilgisi>();
+                List<YaziTipiBilgisi> ParaInfos = new List<YaziTipiBilgisi>();
+                List<SayfaBilgisi> PageInfos = new List<SayfaBilgisi>();
+                List<UrlBilgi> UrlInfos = new List<UrlBilgi>();
+
+
+
+                HeadInfos.Clear();
+                PageInfos.Clear();
+                ParaInfos.Clear();
+                UrlInfos.Clear();
+
+
+                foreach (Section sec in doc.Sections)
+                {
+                   
+                    SayfaBilgisi pageInfo = new SayfaBilgisi();
+                    pageInfo.Left = sec.PageSetup.Margins.Left * 2.54f / 72f;
+                    pageInfo.Right = sec.PageSetup.Margins.Right * 2.54f / 72f;
+                    PageInfos.Add(pageInfo);
+
+                    foreach (DocumentObject obj in sec.Body.ChildObjects)
+                    {
+                        if (obj.DocumentObjectType == DocumentObjectType.Paragraph)
+                        {
+                            Paragraph para = obj as Paragraph;
+                            if (para.StyleName.Contains("Balk1"))   
+                            {
+                                foreach (DocumentObject paraObj in para.ChildObjects)
+                                {
+                                    if (paraObj.DocumentObjectType == DocumentObjectType.TextRange)
+                                    {
+                                        TextRange textRange = paraObj as TextRange;
+                                        YaziTipiBilgisi head = new YaziTipiBilgisi();
+                                        head.FontName = textRange.CharacterFormat.FontName;
+                                        head.FontSize = textRange.CharacterFormat.FontSize;
+                                        head.FontColor = textRange.CharacterFormat.TextColor;
+                                        HeadInfos.Add(head);
+                                    }
+                                }
+                            }
+                            else if (!para.Text.Equals(""))           
+                            {
+                                foreach (DocumentObject paraObj in para.ChildObjects)
+                                {
+                                    if (paraObj.DocumentObjectType == DocumentObjectType.TextRange)
+                                    {
+                                        TextRange textRange = paraObj as TextRange;
+                                        YaziTipiBilgisi parainfo = new YaziTipiBilgisi();
+                                        parainfo.FontName = textRange.CharacterFormat.FontName;
+                                        parainfo.FontSize = textRange.CharacterFormat.FontSize;
+                                        parainfo.FontColor = textRange.CharacterFormat.TextColor;
+                                        ParaInfos.Add(parainfo);
+
+                                    }
+
+                                    if (paraObj.DocumentObjectType == DocumentObjectType.Field)
+                                    {
+                                        Field field = paraObj as Field;
+                                        if (field.Type.Equals(FieldType.FieldHyperlink))
+                                        {
+                                            UrlBilgi urlInfo = new UrlBilgi();
+                                            urlInfo.Url = field.FieldText;
+                                            UrlInfos.Add(urlInfo);
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+
+                string pattern = "[a-zA-z]+://[^\\s]*";
+                Regex rgx = new Regex(pattern);
+                TextSelection[] textSelections = doc.FindAllPattern(rgx);
+
+                if (textSelections != null)
+                {
+                foreach (TextSelection textSelection in textSelections)
+                    {
+                        TextRange textRange = textSelection.GetAsOneRange();
+                        UrlBilgi urlInfo = new UrlBilgi();
+                        urlInfo.Url = textRange.Text;
+                        UrlInfos.Add(urlInfo);
+                    }
+
+                }
+             
+
+                foreach (UrlBilgi urlInfo in UrlInfos)
+                {
+                    string url = urlInfo.Url;
+                    HttpWebRequest req = null;
+                    try
+                    {
+                        req = (HttpWebRequest)WebRequest.CreateDefault(new Uri(url));
+                        req.Method = "HEAD";
+                        req.Timeout = 10000;
+                        HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                        if (Convert.ToInt32(res.StatusCode).ToString().Equals("200"))
+                        {
+                            urlInfo.Message = "Url çalışıyor";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        urlInfo.Message = ex.Message;
+                    }
+                    finally
+                    {
+                        if (req != null)
+                        {
+                            req.Abort();
+                            req = null;
+                        }
+                    }
+                }
+            
+
+
+                bool urlValue = false;
+                foreach (UrlBilgi urlInfo in UrlInfos)
+                {
+                    if (!urlInfo.Message.Equals("URL ÇALIŞIYOR"))
+                    {
+                        urlValue = true;
+                    }
+                }
+                if (urlValue)
+                {
+                   richTextBox1.Text = richTextBox1.Text + "URL TABLOSUNU KONTROL EDİNİZ " +Environment.NewLine;
+                }
+
+                bool paraValue = false;
+                bool paraValueSize = false;
+                foreach (YaziTipiBilgisi fontInfo in ParaInfos)
+                {
+                    if (!fontInfo.FontName.Equals("Times New Roman"))
+                    {
+                        paraValue = true;
+                    }
+                    if (!fontInfo.FontSize.Equals("12"))
+
+                    {
+                        paraValueSize = true;
+                    }
+                   
+                }
+                if (paraValue)
+                {
+                   richTextBox1.Text = richTextBox1.Text+"Paragraflar'da Yazı tipini 'Times New Roman' olarak değiştirin. " + Environment.NewLine;
+                }
+                if (paraValueSize)
+                {
+                    richTextBox1.Text = richTextBox1.Text + "Paragraflar'da Yazı boyutunu '12' olarak değiştirin." + Environment.NewLine;
+                }
+             
+                bool headValue = false;
+                bool headValueSize = false;
+                foreach (YaziTipiBilgisi fontInfo in HeadInfos)
+                {
+                    if (!fontInfo.FontName.Equals("Times New Roman"))
+                    {
+                        headValue = true;
+                    }
+                    if (!fontInfo.FontSize.Equals("14"))
+                    {
+                        headValueSize = true;
+                    }
+                }
+                if (headValue)
+                {
+                    richTextBox1.Text = richTextBox1.Text + "Başlık'ta Yazı tipini 'Times New Roman' olarak değiştirin. " + Environment.NewLine;
+                }
+               if (headValueSize)
+                {
+                    richTextBox1.Text = richTextBox1.Text + "Başlık'ta Yazı boyutunu '14' olarak değiştirin." + Environment.NewLine;
+                }
+            }
+
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            //file.Filter = "Word Dosyası |*.xml| Word Dosyası|*.xml";  
+            file.FilterIndex = 2;
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.Title = "Docx Formatındaki Dosyası Seçiniz..";
+            file.Multiselect = true;
+            // bu kod çoklu seçim yapabilmemizi sağlar.
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string DosyaYolu = file.FileName;
+                string DosyaAdi = file.SafeFileName;
+                labelyol.Text = DosyaYolu;
+            }
+            
+
+        }
+    }
+
+    public class YaziTipiBilgisi
+    {
+        public string FontName { get; set; }
+        public float FontSize { get; set; }
+        public Color FontColor { get; set; }
+    }
+
+    public class SayfaBilgisi
+    {
+        public float Left { get; set; }
+        public float Right { get; set; }
+    }
+
+    public class UrlBilgi
+    {
+        public string Url { get; set; }
+        public string Message { get; set; }
+    }
+
+}
